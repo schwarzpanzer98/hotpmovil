8d98b3c759dafbab6b0903d1d43a0222cd415ba3
Nicolas Estanislau <nicolas_estanislau@hotmail.com>
2021-12-07 14:17:23 -0300

    update infoCourse page

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "18ef3c7db9bb8264"; }
+/******/ 		__webpack_require__.h = function() { return "ef902bec739755c3"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
