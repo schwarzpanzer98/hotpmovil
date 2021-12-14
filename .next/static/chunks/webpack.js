2e088640dac6181b740589bf2a66df8f0942428e
Nicolas Estanislau <nicolas_estanislau@hotmail.com>
2021-12-14 17:35:42 -0300

    wip: ajust sidebar

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "b95218192c66def4"; }
+/******/ 		__webpack_require__.h = function() { return "d7d9090395068d34"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
